255eefa5a52284ee385bb60d90e709cc3fdfb384
Prenzo <crazydan123@hotmail.co.uk>
2014-03-03 11:53:49 +0000

    Create Task.py

---
+++ Task.py
@@ -0,0 +1,19 @@
+print('a' in 'boat')
+#expected outcome is true
+print('c' in 'boat')
+#expected outcome is false
+print('2' + '2')
+#expected outcome is 22
+print(4*'4')
+#expected outcome is 4444
+alphabet= 'abcdefghijklmnopqrstuvwxyz'
+print(len(alphabet))
+#expected outcome is 26
+print(alphabet[1])
+#expected outcome is b
+print(alphabet[4:7])
+#expected outcome is efg
+print(alphabet[0:6:2])
+#expected outcome is ace
+print(alphabet[::-1])
+#expected outcome is zyxwvutsrqponmlkjihgfedcba
